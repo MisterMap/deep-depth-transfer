0d231de9e7d40209b5ba3162f190697b5fb7fa34
Mikhail Kurenkov <Mikhail.Kurenkov@skoltech.ru>
2020-10-06 19:43:01 +0300

    #26: Fix float problem

--- deep_depth_transfer/models/scaled_unsupervised_depth_model.py
+++ deep_depth_transfer/models/scaled_unsupervised_depth_model.py
@@ -6,8 +6,8 @@ class ScaledUnsupervisedDepthModel(UnsupervisedDepthModel):
     def __init__(self, pose_net, depth_net, criterion, optimizer_parameters, result_visualizer=None,
                  *args, **kwargs):
         super().__init__(pose_net, depth_net, criterion, optimizer_parameters, result_visualizer, *args, **kwargs)
-        self._log_min_depth = torch.nn.Parameter(torch.tensor(0))
-        self._log_scale = torch.nn.Parameter(torch.tensor(0))
+        self._log_min_depth = torch.nn.Parameter(torch.tensor(0.))
+        self._log_scale = torch.nn.Parameter(torch.tensor(0.))
 
     def depth(self, x):
         out = self._depth_net(x, is_return_depth=False)
