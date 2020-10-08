a7ae079edd7310ecfe90cd0b56e08d5878ff91c7
Ivan Khokhlov <khokhlov.iyu@gmail.com>
2020-10-08 17:38:29 +0300

    Classes Metric, ValidationDataset and EvaluateMonoDataset. Ref. #2 #16

--- deep_depth_transfer/utils/__init__.py
+++ deep_depth_transfer/utils/__init__.py
@@ -1,3 +1,6 @@
 from .logger_collection import LoggerCollection
 from .tensor_board_logger import TensorBoardLogger
 from .mlflow_logger import MLFlowLogger
+from .metrics import Metric
+from .validation_dataset import ValidationDataset
+from .evaluate_mono_dataset import EvaluateMonoDataset
